a5cf977fd10316abe8e06e950aa5e7f600fdfe7e
ManishaLalage <manishalalage91@gmail.com>
2022-11-07 21:25:00 -0600

    Completed all schemas

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
 
-import dotenv from 'dotenv';
-
-//Database Connection 
+// Database connection
 import ConnectDB from "./database/connection";
 
 dotenv.config();
@@ -11,25 +10,21 @@ const zomato = express();
 
 zomato.use(express.json());
 
-zomato.get('/',(req,res) =>{
-    res.json({
-        message:"Server is running",
-    });
+zomato.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+  });
 });
 
-const PORT = 4003;
+const PORT = 4005;
 
-zomato.listen(PORT,() =>{
-//  ConnectDB()
-//  .then(() =>{
-//     console.log("Server is running !!!");
-//  })
-//  .catch((error) => {
-// console.log("Server is running but database connection failled...");
-// console.log(error); 
-// });
-
-
-console.log("Server is running !!!");
-
+zomato.listen(PORT, () => {
+  ConnectDB()
+    .then(() => {
+      console.log("Server is running !!!");
+    })
+    .catch((error) => {
+      console.log("Server is running, but database connection failed...");
+      console.log(error);
+    });
 });
